01d6f1e25512c1cb5180cdf6f96fbe3424320b26
Cemalkumustekin <105917138+Cemalkumustekin@users.noreply.github.com>
2022-12-22 22:43:33 +0300

    admin dosyasına ek güncellemeler yapıldı.

--- home/admin.py
+++ home/admin.py
@@ -3,6 +3,7 @@ from django.contrib import admin
 from foodsdrinks.models import Category
 class CategoryAdmin(admin.ModelAdmin):
     list_display = ['yemekismi', 'icecekismi']
+    list_filter = ['icecekismi','yemekismi']
 
 
 # Register your models here.
